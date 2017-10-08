b4d3c3f6b48e961a024ada890ca698e740cc5fd4
Ada <gyenesada@caesar.elte.hu>
2017-10-08 17:40:27 +0200

    10.08 new classes

---
+++ src/szakdolgozat/Client/Main.java
@@ -0,0 +1,10 @@
+package szakdolgozat.Client;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        WindowFrame wf = new WindowFrame();
+        wf.setVisible(true);
+    }
+}
---
+++ src/szakdolgozat/Client/WWFController.java
@@ -0,0 +1,12 @@
+package szakdolgozat.Client;
+
+public class WWFController implements Runnable{
+    public WWFController(){
+        
+    }
+    
+    @Override
+    public void run(){
+        
+    }
+}
